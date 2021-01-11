957607c1ed89cfa40bccd385ef9e6c2a9741323f
alonso9v9 <alonso9v9@gmail.com>
2021-01-10 23:05:49 -0600

    Golden reference first approach

--- scoreboard.sv
+++ scoreboard.sv
@@ -6,6 +6,10 @@ class scoreboard extends uvm_scoreboard;
     endfunction
 
     bit [31:0] crrt_Z;
+    bit [23:0] fraction;
+    bit und,over,nan;
+    real fracX,fracY,fracZ;
+    
 
     uvm_analysis_imp #(Item, scoreboard) m_analysis_imp;
 
@@ -18,8 +22,48 @@ class scoreboard extends uvm_scoreboard;
 
     virtual function write(Item item);
 
+        ///////GOLDEN REFERENCE////////
+
+        fracZ=item.fp_X[22:0]+(item.fp_X[22:0]*item.fp_Y[22:0])/8388608+item.fp_Y[22:0];
+        
+        if (fraction[-24]) begin  //It needs rounding
+
+            case (item.r_mode)
+                3'b000:begin  //last bit 0
+                end
+
+                3'b001:begin  //last bit doesnt change (truncate)
+                end
+
+                3'b010:begin  //last bit depends on the sign of the result
+
+
+                end
+
+                3'b011:begin  //last bit depends on the sign of the result
+
+
+                end
+
+                3'b100:begin  //add 1 to the fraction field
+
+                default: begin
+                    fraction=fraction[-1:-23]; //truncate the result
+                end
+            endcase
+
+        end else begin
+            fraction= fraction[-1:-23];
+        end
+
+        und  =( !(item.fp_X[30:23] || item.fp_Y[30:23]) && !(item.fp_X[22:0] || item.fp_Y[22:0]))? 1 : 0;
+        over =(condition )? 1 : 0;
+        nan  =(condition )? 1 : 0;
+
         crrt_Z={item.fp_X[31]^item.fp_Y[31],item.fp_X[30:23]+item.fp_Y[30:23]-127
-        ,item.fp_X[22:0]+item.fp_X[22:0]*item.fp_Y[22:0]+item.fp_Y[22:0]}; //Checker
+        ,fraction}; 
+
+        //////////////////////////////
 
         `uvm_info("SCBD", $sformatf("Mode=%0b Op_x=%0b Op_y=%0b Result=%0b Correct=%0b Overflow=%0b 
         Underflow=%0b", item.r_mode,item.fp_X,item.fp_Y,item.fp_Z,crrt_Z,item.ovrf,item.udrf), UVM_LOW)
@@ -32,7 +76,7 @@ class scoreboard extends uvm_scoreboard;
 
         //UNDERFLOW
         //OVERFLOW
-        //NAN
+        //NAN 
 
     endfunction
 
