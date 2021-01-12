3dc22a50bcb939fbad9ab5cffe8d35f3832f86a9
alonso9v9 <alonso9v9@gmail.com>
2021-01-11 21:17:45 -0600

    golden update

--- scoreboard.sv
+++ scoreboard.sv
@@ -7,7 +7,7 @@ class scoreboard extends uvm_scoreboard;
 
     bit [31:0] crrt_Z;
     bit [32:0] fract_Z_unR; // fracZ without rounding.
-    bit und,over,nan_X,nan_Y,nan_Z;
+    bit und_X,over_X,nan_X,und_Y,over_Y,nan_Y,und_Z,over_Z,nan_Z;
     real fracZ; //fracZ as decimal point data
     
     bit sign_field_Z;
@@ -26,6 +26,16 @@ class scoreboard extends uvm_scoreboard;
     virtual function write(Item item);
 
         ///////GOLDEN REFERENCE////////
+
+
+
+        exp_field_X=item.fp_X[30:23];
+
+        fract_field_X=item.fp_X[22:0];
+
+        exp_field_Y=
+
+        fract_field_Y=
         
         sign_field_Z=item.fp_X[31]^item.fp_Y[31];
 
@@ -39,6 +49,8 @@ class scoreboard extends uvm_scoreboard;
         //Now fract_field_Z should have a lentght of 23 +1 but if it is greater then the result is NaN
 
         if (fract_Z_unR>=2**24) begin
+            
+            nan_Z=0;
 
             fract_field_Z=fract_Z_unR/2; //First the fraction field is truncated
             
@@ -88,18 +100,28 @@ class scoreboard extends uvm_scoreboard;
                 endcase
                 
             end
-
+        end
         else begin
             nan_Z=1;
         end
 
+        und_X  =(!(|exp_field_X) && !(|fract_field_X))? 1 : 0; // If both exp and fract field are 0 then underflow
+        over_X =(&exp_field_X)? 1 : 0; //If exp field is 'd255 then overflow
 
+        und_Y  =(!(|exp_field_Y) && !(|fract_field_Y))? 1 : 0; // If both exp and fract field are 0 then underflow
+        over_Y =(&exp_field_Y)? 1 : 0; //If exp field is 'd255 then overflow
+        
+        und_Z  =(!(|exp_field_Z) && !(|fract_field_Z))? 1 : 0; // If both exp and fract field are 0 then underflow
+        over_Z =(&exp_field_Z)? 1 : 0; //If exp field is 'd255 then overflow
 
-        und  =( !(item.fp_X[30:23] || item.fp_Y[30:23]) && !(item.fp_X[22:0] || item.fp_Y[22:0]))? 1 : 0;
-        over =(condition )? 1 : 0;
-        nan  =(condition )? 1 : 0;
+        nan_X = &exp_field_X & |fract_field_X;
+        nan_Y = &exp_field_Y & |fract_field_Y;
+        nan_Z = &exp_field_Z & |fract_field_Z;
 
-        crrt_Z={sign_field_Z,exp_field_Z,fract_field_Z}; 
+        nan_Z = {und_X & over_Y} | {over_X & und_Y} | nan_Z; //Infinity*0 or 0*Infinity
+        nan_Z = nan_X | nan_Y | nan_Z; //Nan propagation
+
+        crrt_Z={sign_field_Z,exp_field_Z,fract_field_Z};  //correct result
 
         //////////////////////////////
 
